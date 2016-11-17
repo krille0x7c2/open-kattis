7a62b00f692dff82d8b4283879c48dd14ee6d856
Christian Bodelsson <krille0x7c2@gmail.com>
2016-11-17 18:50:24 +0100

    Added oddmanout and functional stacking cups

--- ODDMANOUT/oddmanout.c
+++ ODDMANOUT/oddmanout.c
@@ -1,9 +1,86 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <assert.h>
 
+
+typedef enum {false, true}bool;
+
+size_t input(long int **buffer)
+{
+	int i;
+	long int val;
+	char str[1024];
+	char *ptr = NULL;
+	long *i_buffer = NULL;
+
+	if (!(fgets(str, 1024, stdin)))
+		return -1;
+
+	if (!(i_buffer = calloc(1024, sizeof(*i_buffer))))
+		return -1;
+
+	ptr = str;
+	i = 0;
+	val = 0;
+	
+	for(; *ptr != '\0'; ptr++){
+		if (isdigit(*ptr)) {
+			errno = 0;
+			val = strtol(ptr, &ptr, 10);
+			if ((errno == ERANGE && (val == LONG_MAX ||
+			 val == LONG_MIN)) 						|| 
+			(errno != 0 && (val == 0))) {
+			   free(i_buffer);
+               perror("strtol");
+               exit(EXIT_FAILURE);
+           	}
+           i_buffer[i++] = val;
+		}
+	}
+
+	*buffer = i_buffer;
+	return i;
+}
 int main(int argc, char const *argv[])
 {
+	long int *i_buff = NULL;
+	long int *buff = NULL;
+	long int *rows[50] = {NULL};
+	int i, n, g, k, size;
+	char c;
+
+	scanf("%d", &n);
+	if (n < 1 || n > 50)
+		return 1;
+
+	while((c = getchar()) != '\n' && c != EOF)
+		;
+
+	for (k = 0; k < n; k++){
+		
+		scanf("%d", &g);
+		if (g < 3 || g > 1000)
+			return 1;
+		while((c = getchar()) != '\n' && c != EOF)
+		;
+
+		size = input(&buff);
+		if(!(i_buff = malloc(sizeof(int*) * 1024)))
+			return 1;
+
+		memcpy(i_buff, buff, 1024);
+		free(buff);
+		buff = NULL;
+
+		rows[k] = i_buff;
+
+
 	
+	}
+
 	return 0;
 }
--- STACKINGCUPS/stacking_cups.c
+++ STACKINGCUPS/stacking_cups.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <errno.h>
+#include <limits.h>
 
 struct Cup {
 	int radius;
@@ -35,7 +37,7 @@ char *twist_str(char *str)
 }
 
 
-int bubble_sort (struct Cup *cup, int n) {
+int bubble_sort (struct Cup **cup, int n) {
     int i, t, s = 1;
     char *str_tmp = NULL;
     str_tmp = malloc(sizeof(char) * 30);
@@ -44,106 +46,99 @@ int bubble_sort (struct Cup *cup, int n) {
     while (s) {
         s = 0;
         for (i = 1; i < n; i++) {
-            if ((cup + i)->radius < (cup + i - 1)->radius) {
+            if (cup[i]->radius < cup[i - 1]->radius) {
             	/*Integer*/
-                t = (cup + i)->radius;
-                (cup + i)->radius = (cup + i - 1)->radius;
-                (cup + i - 1)->radius = t;
+                t = cup[i]->radius;
+                cup[i]->radius = cup[i - 1]->radius;
+                cup[i - 1]->radius = t;
                 s = 1;
                 /*Char*/
-                strcpy(str_tmp, (cup + i)->color);
-                strcpy((cup + i)->color, (cup + i - 1)->color);
-                strcpy((cup + i - 1)->color, str_tmp);
+                strcpy(str_tmp, cup[i]->color);
+                strcpy(cup[i]->color, cup[i - 1]->color);
+                strcpy(cup[i - 1]->color, str_tmp);
             }
         }
     }
-   // free(str_tmp);
+
     return 0;
 }
 
+
 int main(int argc, char const *argv[])
 {
-	struct Cup *cup;
-	int n, c, i, j;
-	char *ptr;
+	struct Cup *cup, *cups[20];
+	int n, c, i, j, tmp;
+	char *ptr = NULL;
 	char *str_token = NULL;
+	cup = NULL;
 
 	scanf("%d", &n);
 	if (n < 1 || n > 20)
-		return 1;
+		return 2;
 	while ((c = getchar()) != '\n' && c != EOF)
 		;/*empty*/
 
-	cup = malloc(sizeof(struct Cup) * n);
-	if (cup == NULL)
-		return 1;
-
-	i = 0;
-	do{
+	for (i = 0; i < n; i++){
+		tmp = 0;
 		ptr = NULL;
-		str_token = malloc(sizeof(char) * 30);
-		if (str_token == NULL)
-			return 1;
+		if(!(str_token = malloc(sizeof(char) * 25)))
+			return 4;
 
-		fgets(str_token, 30, stdin);
+		if(!(cup = malloc(sizeof(struct Cup))))
+				return 1;
+
+		fgets(str_token, 25, stdin);
 		str_token[strcspn(str_token, "\n")] = '\0';
 
-		if (strlen(str_token) > 24)
-			return 1;
 
-		if (!isdigit(str_token[0])){
+		if (!isdigit(*str_token)){
 
-			str_token = twist_str(str_token);
-			if (str_token == NULL)
-				return 1;
+			if(!(str_token = twist_str(str_token)))
+				return 6;
 
-			(cup + i)->radius = (int)strtol(str_token, &ptr, 10);
-			if ((cup + i)->radius < 1 || (cup + i)->radius > 999)
-				return 1;
+			errno = 0;
+			tmp = (int)strtol(str_token, &ptr, 10);
+			
+			// if (tmp < 1 || tmp > 999){
+			// 	return 7;
+			// }
+
 			if (*ptr == ' ')
 				ptr++;
-			for (j = 0; j < strlen(ptr); j++){
-				if(!(ptr[j]>='a' && ptr[j]<='z'))
-					return 1;
-			}
-			if (strlen(ptr) > 20)
-				return 1;
 
-			(cup + i)->radius *= 2;
-			strcpy((cup + i)->color, ptr);
-			i++;
-			//free(new_token);
+
+			tmp *= 2;
+			cup->radius = tmp;
+			strcpy(cup->color, ptr);
+			cups[i] = cup;
 			free(str_token);
 			str_token = NULL;
 
 		}else{
 
-		(cup + i)->radius = (int)strtol(str_token, &ptr, 10);
-		if ((cup + i)->radius < 1 || (cup + i)->radius > 999)
-			return 1;
-		if (*ptr == '\0')
-			return 1;
-		if (*ptr == ' ')
-			ptr++;
-		for (j = 0; j < strlen(ptr); j++){
-			if(!(ptr[j]>='a' && ptr[j]<='z'))
-				return 1;
+			tmp = (int)strtol(str_token, &ptr, 10);
+			// if (tmp < 1 || tmp > 999)
+			// 	return 10;
+
+			if (*ptr == ' ')
+				ptr++;
+
+			cup->radius = tmp;
+			strcpy(cup->color, ptr);
+			cups[i] = cup;
+			free(str_token);
+			str_token = NULL;
 		}
-		if (strlen(ptr) > 20)
-			return 1;
+	}
 
-		strcpy((cup + i)->color, ptr);
-		i++;
-		//free(str_token);
+	if (bubble_sort(cups, n))
+		return 14;
+
+	for (i = 0; i < n; i++){
+		if (cups[i]->radius != 0){
+			printf("%s\n", cups[i]->color);
 		}
-	} while (i != n);
-
-	if (bubble_sort(cup, n))
-		return 1;
-
-	for (i = 0; i < n; i++)
-		printf("%s\n", (cup + i)->color);
-
-	//free(cup);
+		
+	}
 	return 0;
 }
