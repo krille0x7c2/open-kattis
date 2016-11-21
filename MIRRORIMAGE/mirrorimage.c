80c42863a14acb11894fcbcc8f5f2da13e3b6ce1
Christian Bodelsson <krille0x7c2@gmail.com>
2016-11-21 10:11:33 +0100

    Working mirror image

--- MIRRORIMAGE/mirrorimage.c
+++ MIRRORIMAGE/mirrorimage.c
@@ -47,17 +47,33 @@ reverse_str ( char *str )
 #		undef XOR_SWAP
 	}
 }		/* -----  end of function reverse_str  ----- */
+ 
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  invert_test
+ *  Description:  
+ * =====================================================================================
+ */
 	void
-mirror ( struct Test *tc )
+invert_test ( struct Test **test )
 {
-	int i,  j;
-	for (i = 0; i < tc->x; i++ ){
-		for (j = 0; j < tc->y; j++){
-
-		}
+	int i, end;
+	struct Test *n_test;
+	if(!(n_test = malloc(sizeof(struct Test))))
+		return;
+	end = (*test)->x - 1;
+	i = 0;
+	while(end >= 0){
+		strncpy(n_test->row[i], (*test)->row[end],  25);
+		n_test->x = (*test)->x;
+		n_test->y = (*test)->y;
+		end--;
+		i++;
 	}
-}		/* -----  end of function mirror  ----- */
+	free(*test);
+	*test = n_test;
+}		/* -----  end of function invert_test  ----- */
 
 	int
 main ( int argc, char *argv[] )
@@ -76,16 +92,15 @@ main ( int argc, char *argv[] )
 			;
 		if (r < 1 || r > 20 || c < 1 || c > 20)
 			return 1;
+		if(!(test = malloc(sizeof(struct Test))))
+			return 1;
 		for (k = 0; k < r; k++ ){
 			if(!(str = malloc(sizeof(char*) * 25)))
 				return 1;
 
-			if(!(test = malloc(sizeof(struct Test))))
-				return 1;
-
-			fgets(str, 20, stdin);
+			fgets(str, 25, stdin);
 			str[strcspn(str, "\n")] = '\0';
-			strncpy(test->row[r], str, 25);
+			strncpy(test->row[k], str, 25);
 			free(str);
 			str = NULL;
 		}
@@ -96,8 +111,19 @@ main ( int argc, char *argv[] )
 
 	for ( i = 0; i < t; i++){
 		for ( j = 0; j < tests[i]->x; j++ ) {
-			printf("%s\n", tests[i]->row[j]);
+			reverse_str(tests[i]->row[j]);
 		}
+	}
+	
+	for (i = 0; i < t; i++)
+		invert_test(&tests[i]);
+		
+	for ( i = 0; i < t; i++){
+		printf("Test %d\n", i + 1);
+		for ( j = 0; j < tests[i]->x; j++ ) {
+			printf("%s\n", tests[i]->row[j]);	
+		}
+		free(tests[i]);
 	}
 	return EXIT_SUCCESS;
 }				/* ---------  end of function main  ---------- */
