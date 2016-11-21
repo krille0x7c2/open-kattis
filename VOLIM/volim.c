4d9580586a8daf1a4ed5f81148b1c45957e95509
Christian Bodelsson <krille0x7c2@gmail.com>
2016-11-21 16:22:11 +0100

    Added sample.in

--- VOLIM/volim.c
+++ VOLIM/volim.c
@@ -23,6 +23,12 @@
 
 struct box {
 	int time_left;
+	char answer;
+};
+
+struct player {
+	struct box b;
+	int time;
 	int label;
 };
 
@@ -37,19 +43,63 @@ static const int TIME_TO_EXPLODE = 210;
 	int
 main ( int argc, char *argv[] )
 {
-	struct box box;
-	int k, n, t;
-	char z;
+	struct player *p;
+	
+	int i, k, n, t, ts[100];
+	char z, cs[100];
+	memset(ts, 0, 100);
+	memset(cs, 0, 100);
 	scanf("%d", &k);
 	if (k < 1 || k > 8)
 		return 1;
 	scanf("%d", &n);
 	if (n < 1 || n > 100)
 		return 1;
-	scanf("%d %c", &t, &z);
-	if (t < 1 || t > 100)
+	if (!(p = malloc(sizeof(struct player)* 8)))
 		return 1;
+	for ( i = 0; i < 8; i++){
+		(p + i)->label = i;
+		(p + i)->time = 0;
+	}
 
+	i = 0;
+	do{
+		scanf("%d %c", &t, &z);
+		if (t < 1 || t > 100)
+			return 1;
+		ts[i] = t;
+		cs[i] = z;
+		i++;
+	} while (i != n);
+	
+	i = k - 1;
+	
+	(p + i)->b.time_left = TIME_TO_EXPLODE;
+	for (;;){
+		switch(cs[i]){
+			case 'T':
+				(p + i)->b.time_left -= ts[i];
+				i++;
+				break;
+			case 'N':
+			case 'P':
+				while(cs[i] != 'T'){
+					(p + i)->b.time_left -= ts[i];
+					if ((p + i)->b.time_left == 0)
+						goto done;
+					i++;
+				}
+				break;
+		}
+	
+		if ((p + i)->b.time_left == 0)
+			break;
+		if ( i == n)
+			i = 0;
+	}
+
+done:
+	printf("%d", (p + i)->label);	
 
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
