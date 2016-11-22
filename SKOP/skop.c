d7d7552db503376a009922e14ddb111a1f4dae6b
Christian Bodelsson <krille0x7c2@gmail.com>
2016-11-22 11:18:57 +0100

    created new problem

---
+++ SKOP/skop.c
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  skop.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/22/2016 11:17:56 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Christian Bodelsson (cb), krille0x7c2@gmail.com
+ *   	  License:  GNU General Public License
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+#include	<stdio.h>
+#include	<string.h>
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
