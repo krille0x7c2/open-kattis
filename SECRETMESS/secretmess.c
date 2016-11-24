a3aea64bf3c21a9d8ae17285e949d41bc6574a0d
Christian Bodelsson <krille0x7c2@gmail.com>
2016-11-24 15:32:06 +0100

    Input implemented and passed all tests

--- SECRETMESS/secretmess.c
+++ SECRETMESS/secretmess.c
@@ -21,6 +21,43 @@
 #include	<stdio.h>
 #include	<string.h>
 
+#define MAX_SIZE 10005
+
+typedef enum {false, true}bool;
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  get_str
+ *  Description:  MAX_SIZE lenght string from stdin
+ * =====================================================================================
+ */
+	int
+get_str ( char **str )
+{
+	int len;
+	char *str_in, *str_new;
+	str_in = str_new = NULL;
+	
+	if (!(str_in = malloc(MAX_SIZE)))
+		return 1;
+	
+	fgets(str_in, MAX_SIZE, stdin);
+	str_in[strcspn(str_in, "\n")] = '\0';
+	
+	len = strlen(str_in);
+
+	if (len < 1 || len > 10000)
+		return 1;
+	
+	if (!(str_new = realloc(str_in, len + 1)))
+		return 1;
+	
+	*str = str_new;
+	
+	return len;
+}		/* -----  end of function get_str  ----- */
+
 /* 
  * ===  FUNCTION  ======================================================================
  *         Name:  main
@@ -30,5 +67,28 @@
 	int
 main ( int argc, char *argv[] )
 {
+	char c, *row = NULL, *rows[100] = {NULL};
+	int i, n;
+
+	scanf("%d", &n);
+	if (n < 1 || n > 100)
+		return 1;
+	/*Clear stdin of trailing newline*/
+	while((c = getchar()) != '\n' && c != EOF)
+		;/*EMPTY */
+	
+	i = 0;
+	do {
+		get_str(&row);
+		rows[i] = row;
+		row = NULL;
+		i++;
+	} while (i != n);
+	
+	/* TODO */
+
+	for (i = 0; i < n; i++){
+		free(rows[i]);
+	}
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
