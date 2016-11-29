6b1021b04c0a78ab85a9712d36fb464acdec49f5
Christian Bodelsson <krille0x7c2@gmail.com>
2016-11-29 11:23:05 +0100

    New problem

---
+++ ANEWALPHABET/anewalphabet.c
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  anewalphabet.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/29/2016 11:22:11 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Christian Bodelsson (cb), krille0x7c2@gmail.com
+ *   	  License:  GNU General Public License
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+#include	<stdio.h>
+#include	<string.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
