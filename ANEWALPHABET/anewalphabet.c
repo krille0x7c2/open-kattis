3dbd0f66f86e607db15d8a779f9df20aa3b449da
Christian Bodelsson <krille0x7c2@gmail.com>
2016-11-29 12:06:41 +0100

    Accepted solution

--- ANEWALPHABET/anewalphabet.c
+++ ANEWALPHABET/anewalphabet.c
@@ -20,6 +20,14 @@
 #include	<stdlib.h>
 #include	<stdio.h>
 #include	<string.h>
+#include	<ctype.h>
+
+char *lookup[] = {
+	"@", "8", "(", "|)", "3", "#", "6", "[-]", 
+	"|", "_|", "|<", "1", "[]\\/[]", "[]\\[]",
+	"0", "|D", "(,)", "|Z", "$", "']['", "|_|", 
+	"\\/", "\\/\\/", "}{", "`/", "2"
+};
 
 /* 
  * ===  FUNCTION  ======================================================================
@@ -30,5 +38,23 @@
 	int
 main ( int argc, char *argv[] )
 {
+	int i;
+	char *tmp, *ptr, str[10005];
+	memset(str, 0, 10005);
+	fgets(str, 10005, stdin);
+	str[strcspn(str, "\n")] = '\0';
+	ptr = str;
+
+	for (; *ptr != '\0'; ptr++){
+		if ((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'Z')){
+			i = tolower(*ptr) - 'a';
+			tmp = lookup[i];
+			for (; *tmp != '\0'; tmp++)
+				printf("%c", *tmp);
+		} else {
+			printf("%c", *ptr);
+		}
+	}
+
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
