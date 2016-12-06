220c41b9e0b8991b99fd87eb560471ecebbbde27
Christian Bodelsson <krille0x7c2@gmail.com>
2016-12-06 11:04:59 +0100

    New problem

---
+++ AAAH/aaah.c
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  aaah.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/06/2016 11:04:23 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Christian Bodelsson (cb), krille0x7c2@gmail.com
+ *   	  License:  GNU General Public License
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+#include	<stdio.h>
+#include	<string.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
