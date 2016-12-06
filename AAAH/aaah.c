d6c19eb26361d12581161117405414bdad588002
Christian Bodelsson <krille0x7c2@gmail.com>
2016-12-06 11:15:33 +0100

    Accepted solution

--- AAAH/aaah.c
+++ AAAH/aaah.c
@@ -20,6 +20,23 @@
 #include	<stdlib.h>
 #include	<stdio.h>
 #include	<string.h>
+typedef enum {false, true}bool;
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  go_to_doc
+ *  Description:  
+ * =====================================================================================
+ */
+	bool
+go_to_doc ( char *str_j, char *str_d )
+{
+	if (strlen(str_d) <= strlen(str_j))
+		return true;
+	else
+		return false;
+}		/* -----  end of function go_to_doc  ----- */
 
 /* 
  * ===  FUNCTION  ======================================================================
@@ -30,5 +47,15 @@
 	int
 main ( int argc, char *argv[] )
 {
+	const char *out[] = {"go", "no"};
+	char jon[1003], doc[1003];
+	fgets(jon, 1001, stdin);
+	fgets(doc, 1001, stdin);
+	
+	if (go_to_doc(jon, doc))
+		printf("%s\n", out[0]);
+	else
+		printf("%s\n", out[1]);
+
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
